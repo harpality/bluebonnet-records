5c3e82626fab9c261ae14c2e3c97408117c69438
capndave <capndavet@gmail.com>
2019-03-08 13:51:32 -0600

    Working on adding sdk

---
+++ config.js
@@ -0,0 +1,3 @@
+export const config = { 
+  ACCESS_KEY: 'Zkj9Gcvit7AAAAAAAAAAEHO1ZRFNxO0kskFwcxWqkkb7VUO6KvnRGkbVVPhGwkNP'  
+}
---
+++ index.js
@@ -0,0 +1,21 @@
+
+// import axios from './node_modules/axios/dist/axios.min.js'
+// import Dropbox './node_modules/dropbox/dist/Dropbox-sdk.min.js'
+import Dropbox from "https://unpkg.com/dropbox/dist/DropboxTeam-sdk.min.js"
+
+import { config } from './config.js'
+console.log(Dropbox)
+
+// const dbx = new Dropbox.Dropbox({ accessToken: config.ACCESS_KEY })
+// dbx.filesListFolder({path: ""})
+// .then(function(response) {
+//     console.log(JSON.stringify(response, null, 4));
+// })
+// .catch(function(error) {
+//     console.error(error);
+// });
+
+
+
+
+
