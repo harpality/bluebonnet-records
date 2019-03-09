c1df87fdb7242fbf527261086765762b4c21d632
capndave <capndavet@gmail.com>
2019-03-09 13:32:45 -0600

    Converted project to webpack

---
+++ src/index.js
@@ -0,0 +1,41 @@
+// import axios from './node_modules/axios/dist/axios.min.js'
+// import Dropbox './node_modules/dropbox/dist/Dropbox-sdk.min.js'
+// import Dropbox from "https://unpkg.com/dropbox/dist/DropboxTeam-sdk.min.js"
+import axios from 'axios'
+
+console.log('Hello World')
+
+// import {config} from './config.js'
+// import marked from './node_modules/marked/marked.min.js'
+
+// console.log(window.Dropbox.Dropbox)
+
+// const dbx = new Dropbox.Dropbox({
+//   accessToken: config.ACCESS_KEY,
+//   fetch: window.fetch,
+// })
+
+// dbx
+//   .filesListFolder({path: ''})
+//   .then(function(response) {
+//     console.log(JSON.stringify(response, null, 4))
+//   })
+//   .catch(function(e) {
+//     console.error(e)
+//   })
+
+// dbx
+//   .filesDownload({path: '/ANNOUNCEMENTS.md'})
+//   .then(function(response) {
+//     var blob = response.fileBlob
+//     var reader = new FileReader()
+//     reader.addEventListener('loadend', function() {
+//       console.log(reader.result) // will print out file content
+//     })
+//     reader.readAsText(blob)
+//   })
+//   .catch(function(e) {
+//     console.error(e)
+//   })
+
+// console.log(marked('#Some Text'))
