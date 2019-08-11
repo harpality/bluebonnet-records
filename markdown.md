38159f7f417299a1042fd1fd3b9b307dd4a62b2c
capndave <capndavet@gmail.com>
2019-08-11 12:21:20 -0500

    Added js function to make all content hidden until everything is loaded

---
+++ markdown.md
@@ -0,0 +1,1 @@
+** Come on in! ** The water's fine
--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import marked from 'marked'
 import {config} from '../config.js'
 import './index.css'
+
+document.onreadystatechange = function () {
+  var state = document.readyState
+  if (state == 'complete') {
+       // document.getElementById('load').style.visibility="hidden"
+       document.getElementById('main').style.visibility="visible"
+  }
+}
+
+fetch('https://drive.google.com/open?id=1D-1pwbK80e3f6bpuG2c5Kti-ItH-JQpZ')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(JSON.stringify(myJson));
+  })
+  .catch(e => console.log(e))
