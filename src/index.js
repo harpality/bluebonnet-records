bf7b7e53543f10ffc11d5da09e578022837769e0
capndave <capndavet@gmail.com>
2019-08-11 22:15:47 -0500

    successfully getting markdown from google drive

--- src/index.js
+++ src/index.js
@@ -1,47 +1,58 @@
 import marked from 'marked'
-import {config} from '../config.js'
+import { config } from '../config.js'
 import './index.css'
 
-// document.onreadystatechange = function () {
-//   var state = document.readyState
-//   if (state == 'complete') {
-//        // document.getElementById('load').style.visibility="hidden"
-//        document.getElementById('main').style.visibility="visible"
-//   }
-// }
+document.onreadystatechange = function () {
+    var state = document.readyState
+    if (state == 'complete') {
+        // document.getElementById('load').style.visibility="hidden"
+        document.getElementById('main').style.visibility = "visible"
+    }
+}
 
-//   const id = '1D-1pwbK80e3f6bpuG2c5Kti-ItH-JQpZ'
+console.log('hello world')
 
-//   function get_doc(id){
-//     const url = 'https://www.googleapis.com/drive/v3/files/'+id+'?alt=media'
+const id ='1VNr1aQHwS9lM_pJmVGreBDnB8_qPlzwh'
 
-//     if(self.fetch){
-//     var setHeaders = new Headers();
-//     setHeaders.append('Authorization', 'Bearer ' + authToken.access_token);
-//     setHeaders.append('Content-Type', mime);
+function get_doc(id) {
+    const url = 'https://www.googleapis.com/drive/v3/files/' + id + '?alt=media'
 
-//     var setOptions = {
-//         method: 'GET',
-//         headers: setHeaders
-//     };
+    console.log(self.fetch)
+    const authToken = {
+        access_token: "ya29.GltiBwckko_vkbqpgZtS9qbW0Jg8pRF_5SIt_A-2Xqc7UnXFP736q4GosqczLMKT0zvEc-w0dD0pYbnylBlzThcTyNNk8MtGIJQQ6wuLoBkTuSLN65vHdZ5kMGfN"
+    }
+    const mime = 'application/vnd.google-apps.file'
 
-//     fetch(url,setOptions)
-//         .then(response => { if(response.ok){
-//         var reader = response.body.getReader();
-//         var decoder = new TextDecoder();
-//         reader.read().then(function(result){
-//             var data = {}
-//             data = decoder.decode(result.value, {stream: !result.done});
-//             console.log(data);
-//     });
-//         }
-//     else{
-//         console.log("Response wast not ok");
-//     }
-//   })  .catch(error => {
-//         console.log("There is an error " + error.message);
-//         });
-//     }
-// }
+    if (self.fetch) {
+        var setHeaders = new Headers();
+        setHeaders.append('Authorization', 'Bearer ' + authToken.access_token);
+        setHeaders.append('Content-Type', mime);
 
-// get_doc(id)
+        var setOptions = {
+            method: 'GET',
+            headers: setHeaders
+        };
+
+        fetch(url, setOptions)
+            .then(response => {
+                if (response.ok) {
+                    var reader = response.body.getReader();
+                    var decoder = new TextDecoder();
+                    reader.read().then(function (result) {
+                        var data = {}
+                        data = decoder.decode(result.value, { stream: !result.done });
+                        // Add to announcement-text
+                        console.log(data)
+                        document.getElementById('announcement-text').innerHTML = data
+                    });
+                }
+                else {
+                    console.log("Response wast not ok");
+                }
+            }).catch(error => {
+                console.log("There is an error " + error.message);
+            });
+    }
+}
+
+get_doc(id)
